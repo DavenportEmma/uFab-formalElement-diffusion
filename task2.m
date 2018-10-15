d1f615c720da013b53b7690c330f274eeb6328f9
Conor <43864112+ConorDavenport@users.noreply.github.com>
2018-10-15 19:53:10 +0100

    completed task 2

---
+++ task2.m
@@ -0,0 +1,14 @@
+C_vector = zeros(1,100);
+C_vector(1:2) = 2e19;
+
+plot(C_vector);
+xlabel('Depth');
+ylabel('Concentration');
+hold on;
+
+C_vector(1) = 0.5*(0 + C_vector(2));
+for i = 2:length(C_vector)-1  % start at 2 because there is no value left of the first element
+    C_vector(i) = 0.5*(C_vector(i-1) + C_vector(i+1));
+end
+
+plot(C_vector);
