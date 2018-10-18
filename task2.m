4c632e6135f816ef8dd14efee83c27079b37b97e
Lloyd Mundo <lloyddenver@gmail.com>
2018-10-18 15:07:06 +0100

    changed initial concentration

--- task2.m
+++ task2.m
@@ -6,7 +6,7 @@ xlabel('Depth');
 ylabel('Concentration');
 hold on;
 
-C_vector(1) = 0.5*(0 + C_vector(2));
+C_vector(1) = 0.5*((2*10^19) + C_vector(2));
 for i = 2:length(C_vector)-1  % start at 2 because there is no value left of the first element
     C_vector(i) = 0.5*(C_vector(i-1) + C_vector(i+1));
 end
