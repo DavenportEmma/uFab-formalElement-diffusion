dc87c7d4e8739e591dd4fc02eb18aefc29a0f7a1
Lloyd Mundo <lloyddenver@gmail.com>
2018-11-03 16:14:32 +0000

    added semilogy in comments if needed

--- task6.m
+++ task6.m
@@ -9,7 +9,8 @@ dx = 6e-7;     %chose this number so k < 1/2
 k = D*dt/(dx^2);
 
 X_vector = (0:dx:(length(C_vector)*dx)-dx);
-
+    
+% semilogy(X_vector, C_vector);
 plot(X_vector, C_vector);
 xlabel('Depth (cm)');
 ylabel('Concentration (/cm^3)');
@@ -21,7 +22,8 @@ for j = 1:1000
         C_vector(i) = C_vector(i) + k*(C_vector(i-1) - 2*C_vector(i) + C_vector(i+1));
     end
 %    pause(0.005)
-%    semilogy(0:length(C_vector) -1, C_vector);
+%    semilogy(X_vector, C_vector);
 %    plot(X_vector, C_vector);
 end
 plot(X_vector, C_vector);
+% semilogy(X_vector, C_vector);
